85a3f090a24bffc654960acfcc40dc2a92bb5afb
Timo <Timo@LAPTOP-MTBLPAJA.fritz.box>
2020-04-16 12:45:33 +0200

    konvertieren zu char

--- src/DateiEinlesen.java
+++ src/DateiEinlesen.java
@@ -8,10 +8,23 @@ public class DateiEinlesen {
 		File daten = new File("TPicSim1.LST");
 		
 		Scanner scan = new Scanner(daten);
-		while(scan.hasNextLine()) {
-			System.out.println(scan.nextLine());
+		while(scan.hasNextLine()) { //Prüft ob es eine nächste Zeile gibt, die noch nicht gelesen wurde
+			
+			String s1 = scan.nextLine();
+			char char0 = s1.charAt(0);
+			char char1 = s1.charAt(1);
+			char char2 = s1.charAt(2);
+			char char3 = s1.charAt(3);
+			char char4 = s1.charAt(5);
+			char char5 = s1.charAt(6);
+			char char6 = s1.charAt(7);
+			char char7 = s1.charAt(8);
+			
+			String address0 = String.valueOf(char0);
+			
+			
 		}
-		scan.close();
+		scan.close(); //Falls die while() bedingung oben nicht erfüllt, wird der Scanvorgang beendet.
 	}
 	
 
