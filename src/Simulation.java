689d4d412defba10e81ef3d25aa584b0947dcd5d
MarcL <MarcL@LAPTOP-IR9JBPBM.fritz.box>
2020-05-14 09:54:48 +0200

    Befehlsdecoder

--- src/Simulation.java
+++ src/Simulation.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class Simulation {
 
 	public static void main(String[] args) throws FileNotFoundException {
-		int[] befehlsspeicher = new int[1024]; // Befehlsspeicher mit der Größe 1024.
+		long[] befehlsspeicher = new long[1024]; // Befehlsspeicher mit der Größe 1024.
 		int[] datenspeicher = new int[256]; // Datenspeicher mit der Größe 256.
-		File daten = new File("TPicSim1.LST"); // Eine manuell ausgewählte Datei wird dem Programm hinzugefügt.
+		File daten = new File("TPicSim2.LST"); // Eine manuell ausgewählte Datei wird dem Programm hinzugefügt.
 
 		Scanner scan = new Scanner(daten);
 		while (scan.hasNextLine()) { // Prüft ob es eine nächste Zeile gibt, die noch nicht gelesen wurde
@@ -21,132 +21,135 @@ public class Simulation {
 
 			} else {
 
-				String sAdresse = s1.substring(0,4);
-				String Adresse = ("0x" + sAdresse);	
+				String sAdresse = s1.substring(0, 4);
+				String Adresse = ("0x" + sAdresse);
 				int iAdresse = Integer.parseInt(sAdresse, 16);
-				String sBefehl = s1.substring(5,9);
-				int Befehl = Integer.parseInt(sBefehl, 16);
+				String sBefehl = s1.substring(5, 9);
+				long Befehl = Integer.parseInt(sBefehl, 16);
 				String s = "";
-				while(Befehl > 0) {
-					int temp = (Befehl & 1);
-					if(temp != 0) {
+				while (Befehl > 0) {
+					long temp = (Befehl & 1);
+					if (temp != 0) {
 						s = "1" + s;
-					}
-					else {
+					} else {
 						s = "0" + s;
-						
-						
-					}Befehl >>= 1;
+
+					}
+					Befehl >>= 1;
 				}
-				
-				//System.out.println(s);
-				Befehl = Integer.parseInt(s);
-				befehlsspeicher[iAdresse] = Befehl;
+				int counter = 0;
+				for (int i = s.length(); i > 0; i--) {
+					if (s.charAt(counter) == '1') {
+						long temp = 1;
+						for (int j = i; j > 1; j--) {
+							temp = temp * 10;
+						}
+						int val = 0;
+						if (s.charAt(counter) == '1') {
+							val = 1;
+						}
+						Befehl = Befehl + (val * temp);
+					}
+					counter++;
+				}
+				// System.out.println(s);
+				// Befehl = Integer.parseInt(s);
 			}
 
 		}
 		scan.close(); // Falls die while() bedingung oben nicht erfüllt, wird der Scanvorgang beendet.
 
-		System.out.println(befehlsspeicher[0]);
+		System.out.println(befehlsspeicher[7]);
 		for (int i = 0; i < befehlsspeicher.length; i++) {
-			int befehl = befehlsspeicher[i];
-			
-			//if ((befehl >= 0x11000000000000) && (befehl <= 0x11001111111111)) {
-				//System.out.println("movlw");
-				//} 
-			//nop 0x00 0000 0000 0000
-			   // 0x00 0000 0010 0000
-			   // 0x00 0000 0100 0000
-			    //0x00 0000 0110 0000
-			if (befehlsspeicher[i] == 1) { // CLRF
-				int operanden = datenspeicher[i];
-				
-			}
-			else if (befehlsspeicher[i] == 2) { // SUBWF
+			long befehl = befehlsspeicher[i];
 
-			}
-			else if (befehlsspeicher[i] == 3) { // DECF
-
-			}
-			else if (befehlsspeicher[i] == 4) { // IORWF
-
-			}
-			else if (befehlsspeicher[i] == 5) { // ANDWF
-
-			}
-			else if (befehlsspeicher[i] == 6) { // XORWF
-
-			}
-			else if (befehlsspeicher[i] == 7) { // ADDWF
-
-			}
-			else if (befehlsspeicher[i] == 8) { // MOVF
-
-			}
-			else if (befehlsspeicher[i] == 9) { // COMF
-
-			}
-			else if (befehlsspeicher[i] == 10) { // INCF
-
-			}
-			else if (befehlsspeicher[i] == 11) { // DECFSZ
-
-			}
-			else if (befehlsspeicher[i] == 12) { // RRF
-
-			}
-			else if (befehlsspeicher[i] == 13) { // RLF
-
-			}
-			else if (befehlsspeicher[i] == 14) { // SWAPF
-
-			}
-			else if (befehlsspeicher[i] == 15) { // INCFSZ
-
-			}
-			else if (befehlsspeicher[i] == 16) { // BCF
-
-			}
-			else if (befehlsspeicher[i] == 23) { // BSF
-
-			}
-			else if (befehlsspeicher[i] == 24) { // BTFSC
-
-			}
-			else if (befehlsspeicher[i] == 29) { // BTFSS
-
-			}
-			else if (befehlsspeicher[i] == 32) { // CALL
-
-			}
-			else if (befehlsspeicher[i] == 40) { // GOTO
-
-			}
-			else if (befehlsspeicher[i] == 48) { // MOVLW
-
-			}
-			else if (befehlsspeicher[i] == 52) { // RETLW
-
-			}
-			else if (befehlsspeicher[i] == 56) { // IORLW
-
-			}
-			else if (befehlsspeicher[i] == 57) { // ANDLW
-
-			}
-			else if (befehlsspeicher[i] == 58) { // XORLW
-
-			}
-			else if (befehlsspeicher[i] == 60) { // SUBLW
-
-			}
-			else if (befehlsspeicher[i] == 62) { // ANDLW
-
-			}
-			else {
-				
+			if ((befehl >= 11000000000000L) && (befehl <= 11001111111111L)) {
+				System.out.println("movlw");
 			}
 
+			else if ((befehl == 0) || (befehl == 100000L) ||
+			(befehl == 1000000L)
+			|| (befehl == 1100000L)) {
+			System.out.println("nop");
+			}
+
+			// else if (befehlsspeicher[i] == 1) { // CLRF
+			// int operanden = datenspeicher[i];
+
+			// }
+			// else if (befehlsspeicher[i] == 2) { // SUBWF
+
+			// }
+			// else if (befehlsspeicher[i] == 3) { // DECF
+
+			// }
+			// else if (befehlsspeicher[i] == 4) { // IORWF
+
+			// }
+			// else if (befehlsspeicher[i] == 5) { // ANDWF
+
+			// }
+			// else if (befehlsspeicher[i] == 6) { // XORWF
+
+			// }
+			// else if (befehlsspeicher[i] == 7) { // ADDWF
+
+			// }
+			// else if (befehlsspeicher[i] == 8) { // MOVF
+
+			// }
+			// else if (befehlsspeicher[i] == 9) { // COMF
+
+			// }
+			// else if (befehlsspeicher[i] == 10) { // INCF
+
+			// }
+			// else if (befehlsspeicher[i] == 11) { // DECFSZ
+
+			// }
+			// else if (befehlsspeicher[i] == 12) { // RRF
+
+			// }
+			// else if (befehlsspeicher[i] == 13) { // RLF
+
+			// }
+			// else if (befehlsspeicher[i] == 14) { // SWAPF
+
+			// }
+			// else if (befehlsspeicher[i] == 15) { // INCFSZ
+
+			// }
+			// else if (befehlsspeicher[i] == 16) { // BCF
+
+			// }
+			// else if (befehlsspeicher[i] == 23) { // BSF
+
+			// }
+			// else if (befehlsspeicher[i] == 24) { // BTFSC
+
+			// }
+			// else if (befehlsspeicher[i] == 29) { // BTFSS
+
+			// }
+			else if ((befehl >= 10000000000000L) && (befehl <= 10011111111111L)) {
+				System.out.println("call");
+			} else if ((befehl >= 10100000000000L) && (befehl <= 10111111111111L)) {
+				System.out.println("goto");
+			} else if ((befehl >= 11010000000000L) && (befehl <= 11011111111111L)) { // RETLW
+				System.out.println("retlw");
+			} else if ((befehl >= 11100000000000L) && (befehl <= 11100011111111L)) {
+				System.out.println("iorlw");
+			} else if ((befehl >= 11100100000000L) && (befehl <= 11100111111111L)) {
+				System.out.println("andlw");
+			} else if ((befehl >= 11101000000000L) && (befehl <= 11101011111111L)) {
+				System.out.println("xorlw");
+			} else if ((befehl >= 11110000000000L) && (befehl <= 11110111111111L)) {
+				System.out.println("sublw");
+			} else if ((befehl >= 11111000000000L) && (befehl <= 11111111111111L)) {
+				System.out.println("addlw");
+			} else if (befehl == 1000L) {
+				System.out.println("return");
+			}
 		}
 	}
 
